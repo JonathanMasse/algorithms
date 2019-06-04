d2ff5b7c2b0f60cb676d182fbe69d499a4a54992
JonathanMasse <jfmasse@my.uri.edu>
2019-06-04 19:54:20 -0400

    test push

--- source/string.cpp
+++ source/string.cpp
@@ -1,14 +1,19 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "string.hpp"
+#include <iostream>
 
-int len = 0;
+//throw "Any Message"
 
 String::String(){
+    //len = 0;
+    //array = NULL;
+    array = new char[1];
+    array[0] = '\0';
     len = 0;
-    array = NULL;
 }
 
 String::String(char* c){
+    /*
     len = 0;
     while(c[len] != '\0'){
         len++;
@@ -17,32 +22,47 @@ String::String(char* c){
     for(int i = 0; i<len;i++){
         array[i] = c[i];
     }
+    */
+    int length = 0;
+    while (c[length])
+        ++length;
+    array = new char[length+1];
+    for(int i = 0; i<length; i++){
+        array[i] = c[i];
+        len = length;
+    }
 }
 
 String::String(char c){
+    //array[0] = c;
+    //len = 1;
+    array = new char[2];
     array[0] = c;
+    array[1] = '\0';
     len = 1;
 }
 
 String::~String(){
-    if(len > 0){
-        delete[] array;
-    }
+    delete[] array;
 }
 
 //O(n)
 char String::at(int x) const{
-    if(x < len){
-        return array[x-1];
+    std::cout << x << std::endl;
+    std::cout << len << std::endl;
+    if(x >= 0 && x < len){
+        std::cout << array[x] <<std::endl;
+        return array[x];
     }
     else{
-         return NULL;
+        //throw exception
+        throw "Throw an exception";
     }
 }
 
 //O(1)
 bool String::empty() const{
-    if(len == 0){
+    if(size() == 0){
         return true;
     }
     else{
@@ -52,7 +72,11 @@ bool String::empty() const{
 
 //O(1)
 unsigned int String::size() const{
-    return len;
+   int count = 0;
+   while(array[count] != '\0'){
+       count++;
+   }
+    return count;
 }
 
 //O(1)
@@ -67,7 +91,7 @@ void String::reserve(unsigned int x){
         array2[i] = array[i];
     }
     for(int j = 0; j<x; j++){
-        array2[j + len] = NULL;
+        array2[j + len] = '\0';
     }
     array = array2;
 }
@@ -88,42 +112,79 @@ void String::insert(char c, int x){
 void String::erase(char c){
     for(int i = 0; i <len; i++){
         if(array[i] == c){
-            array[i] = NULL;
+            array[i] = '\0';
         }
     }
 }
 
 void String::remove(int x){
-    array[x] =
+    //array[x] = NULL;
+    for(int i = x; i<len-1; i++){
+        array[i] = array[i+1];
+    }
+    array[len-1] = '\0';
 }
 
 void String::append(char c){
-    char* array2 = new char[len];
+    char* array2 = new char[len+1];
     for(int i = 0; i<len;i++){
         array2[i] = array[i];
     }
     array2[len] = c;
+    len++;
+    delete array;
     array = array2;
 }
 
 void String::prepend(char c){
-
+    char* array2 = new char[len+1];
+    array2[0] = c;
+    for(int i = 1; i<len+1;i++){
+        array2[i] = array[i];
+    }
+    array = array2;
 }
 
 bool String::compare(char* c) const{
+    for(int i = 0; i<len; i++){
+        if(array[i] != c[i]){
+            return false;
+        }
+    }
     return true;
 }
-
-bool String::compare(String str) const{
-    return true;
+//return compare(str.array)
+bool String::compare(String& str) const{
+   /* bool test = false;
+    for(int i = 0; i<len; i++){
+        // don't know what a String& is
+        if(array[i] != str.array[i]){
+            return test;
+        }
+    }
+    return true;*/
+    return compare(str.array);
 }
 
-void String::concatenate(char*){
-
+void String::concatenate(char* c){
+    int j = 0;
+    while(c[j] != '\0'){
+        j++;
+    }
+    char* array2 = new char[len + j];
+    for(int i = 0; i<len; i++){
+        array2[i] = array[i];
+    }
+    for(int i = len; i<len + j; i++){
+        array2[i] = c[i - len];
+    }
+    len = len + j;
+    delete array;
+    array = array2;
 }
 
-void String::concatenate(String str){
-
+void String::concatenate(String& str){
+    return concatenate(str.array);
 }
 
 unsigned int String::find(char* c, int x) const{
@@ -131,19 +192,33 @@ unsigned int String::find(char* c, int x) const{
 }
 
 unsigned int String::find(char c, int x) const{
-    return 4;
+    /* for(int i = 0; i<len; i++){
+        if(array[i] == c){
+            return i;
+        }
+        else{
+            return len;
+        }
+    }
+    */
 }
 
-unsigned int String::find(String str, int x) const{
+unsigned int String::find(String& str, int x) const{
     return 4;
 }
 
 void String::reverse(){
-
+    char* array2 = new char[len];
+    for(int i = 0; i<len; i++){
+        array2[i] = array[len-i-1];
+    }
+    array = array2;
 }
 
-void String::shift(int){
-
+void String::shift(int x){
+    for(int i = 0; i<len; i++){
+        array[i] += x;
+    }
 }
 
 int String::toInt() const{
@@ -151,5 +226,10 @@ int String::toInt() const{
 }
 
 String String::substr(int x, int y) const{
-    return "hello";
+    int z = y - x + 1;
+    char* array2 = new char[z];
+    for(int i = 0; i<z; i++){
+        array2[i] = array[i+x];
+    }
+    return array2;
 }
